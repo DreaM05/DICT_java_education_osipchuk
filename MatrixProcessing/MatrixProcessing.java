9c22570f773c00151f3e2158fbf6c4d06539d49d
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-21 05:47:29 +0200

    MatrixProcessing - 4-th stage

--- MatrixProcessing/MatrixProcessing.java
+++ MatrixProcessing/MatrixProcessing.java
@@ -12,16 +12,17 @@ public class MatrixProcessing {
         int[][] matrica3;
         int[][] matrica;
         for (; ; ) {
-            System.out.println("1.Сумма матриц\n"+
-                    "2.Умножение на константу\n"+
-                    "3.Умножение матриц\n"+
-                    "4.Выход");
+            System.out.println("1. Сумма матриц\n" +
+                    "2. Умножение на константу\n" +
+                    "3. Умножение матриц\n" +
+                    "4. Транспортировка\n" +
+                    "5. Выход");
             System.out.print("Ваш выбор:");
             a = in.nextInt();
-            if (a==4){
+            if (a == 5){
                 System.exit(0);
             }
-            System.out.print("Введите длинну и ширину матриц:");
+            System.out.print("Введите длину и ширину матрицы: ");
             razmer[0] = in.nextInt();
             razmer[1] = in.nextInt();
             matrica = new int[razmer[0]][razmer[1]];
@@ -46,7 +47,7 @@ public class MatrixProcessing {
                     }
                     for (int q = 0; q < razmer[0]; q++) {
                         for (int w = 0; w < razmer[1]; w++) {
-                            System.out.print("|" + matrica2[q][w] + "|");
+                            System.out.print("|" + matrica3[q][w] + "|");
                         }
                         System.out.println();
                     }
@@ -66,24 +67,24 @@ public class MatrixProcessing {
                     }
                 }else if (a==3){
                     System.out.print("Введите размер второй матрицы:");
-                    razmer[0] = in.nextInt();
-                    razmer[1] = in.nextInt();
+                    razmer2[0] = in.nextInt();
+                    razmer2[1] = in.nextInt();
                     matrica2 = new int [razmer[0]][razmer[1]];
                     System.out.println("Введите вторую матрицу:");
-                    for (int i = 0; i < razmer[0]; i++) {
-                        for (int i1 = 0; i1 < razmer[1]; i1++) {
+                    for (int i = 0; i < razmer2[0]; i++) {
+                        for (int i1 = 0; i1 < razmer2[1]; i1++) {
                             matrica2[i][i1] = in.nextInt();
                         }
                     }
-                    matrica3 = new int [razmer[0]][razmer[1]];
+                    matrica3 = new int[razmer[0]][razmer2[1]];
                     for (int q = 0; q < razmer[0]; q++) {
-                        for (int w = 0; w < razmer[1]; w++) {
+                        for (int w = 0; w < razmer2[1]; w++) {
                             matrica3[q][w] = 0;
                         }
                     }
                     if (razmer[1] == razmer2[0]) {
                         for (int q = 0; q < razmer[0]; q++) {
-                            for (int w = 0; w < razmer[1]; w++) {
+                            for (int w = 0; w < razmer2[1]; w++) {
                                 for (int e = 0; e < razmer[1]; e++) {
                                     matrica3[q][w] = matrica3[q][w] + matrica[q][e]* matrica2[e][w];
                                 }
@@ -91,11 +92,70 @@ public class MatrixProcessing {
                         }
                         for (int q = 0; q < razmer[0]; q++) {
                             for (int w = 0; w < razmer[1]; w++) {
+                                System.out.print("|" + matrica3[q][w] + "|");
+                            }
+                            System.out.println();
+                        }
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                } else if (a == 4) {
+                    System.out.println("1. Главная диагональ\n2. Побочная диагональ\n3. Вертикаль\n4. Горизонталь");
+                    a = in.nextInt();
+                    matrica2 = new int[razmer[1]][razmer[0]];
+                    matrica3 = new int[razmer[0]][razmer[1]];
+                    if (a == 1) {
+                        for (int q = 0; q < razmer[1]; ++q) {
+                            for (int w = 0; w < razmer[0]; ++w) {
+                                matrica2[q][w] = matrica[w][q];
+                            }
+                        }
+                        for (int q = 0; q < razmer[1]; ++q) {
+                            for (int w = 0; w < razmer[0]; ++w) {
                                 System.out.print("|" + matrica2[q][w] + "|");
                             }
                             System.out.println();
                         }
-                    }else {
+                    } else if (a == 2) {
+                        for (int q = 0; q < razmer[0]; ++q) {
+                            for (int w = 0; w < razmer[1]; ++w) {
+                                matrica2[w][q] = matrica[razmer[0] - (q + 1)][razmer[1] - (w + 1)];
+                            }
+                        }
+                        for (int q = 0; q < razmer[1]; ++q) {
+                            for (int w = 0; w < razmer[0]; ++w) {
+                            }
+                                System.out.print("|" + matrica2[q][w] + "|");
+                            }
+                        System.out.println();
+                    }
+                } else if (a == 3) {
+                    for (int q = 0; q < razmer[0]; ++q) {
+                        for (int w = 0; w < razmer[1]; ++w) {
+                            matrica3[q][w] = matrica[q][razmer[1] - (w + 1)];
+                        }
+                        System.out.println();
+                    }
+                    for (int q = 0; q < razmer[0]; ++q) {
+                        for (int w = 0; w < razmer[1]; ++w) {
+                            System.out.print("|" + matrica3[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                } else if (a == 4) {
+                    for (int q = 0; q < razmer[0]; ++q) {
+                        for (int w = 0; w < razmer[1]; ++w) {
+                            matrica3[q][w] = matrica[razmer[0] - (q + 1)][w];
+                        }
+                            System.out.println();
+                        }
+                    for (int q = 0; q < razmer[0]; ++q) {
+                        for (int w = 0; w < razmer[1]; ++w) {
+                            System.out.print("|" + matrica3[q][w] + "|");
+                        }
+                        System.out.println();
+                    }
+                } else {
                         System.out.println("ERROR");
                     }
                 }else {
